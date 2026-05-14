ef8340461e371ab669da62a1a66933e6df738c7b
mojopriest <jari.realestate@gmail.com>
2026-05-14 13:33:39 +0300

    games

--- jp/HTML/jyvapala/example.php
+++
@@ -1,5 +0,0 @@
-<?php include_once("include/puzzle-list.php");
-
-$imageList = new PuzzleList("images/puzzle");
-
-$imageList->createPuzzleList("index.php", "canvas-puzzle-list");
--- jp/HTML/jyvapala/source/build.sh
+++
@@ -1,2 +0,0 @@
-cat utils.js jigsaw-objects.js jigsaw-controller.js jigsaw-ui.js jigsaw-events.js modal-window.js loader.js | uglifyjs > ../js/canvas-puzzle.min.js
-uglifyjs jigsaw-objects-ie.js > ../js/canvas-puzzle.ie.min.js
