eb6edef17d6c48a11ae8c14190ddf7e71d008b8d
mojopriest <jari.realestate@gmail.com>
2026-05-17 20:18:38 +0300

    vv

---
+++ jyvala_piilosana/build/nls/wordsearch_orig.js
@@ -0,0 +1,43 @@
+define({
+        'Pisteet %d':
+        'Pisteet %d',
+    
+        'Ohje':
+        'Ohje',
+        
+        'Uudelleen':
+        'Uudelleen',
+		
+		'Download':
+        'Download',
+       
+        'ratkaisu':
+        'ratkaisu',
+        
+        'Sulje':
+        'Sulje',
+
+        'Hienoa!':
+        'Hienoa!',
+
+        'Pisteesi':
+        'Pisteesi',
+
+        'Ohje':
+        'Ohje',
+		
+		'Etsi kaikki piilosanat ruudukosta.':
+        'Etsi kaikki piilosanat ruudukosta.',
+		
+		'Nimet ovat ruudukossa pysty- tai vaakasuorassa vinottain tai takaperin.':
+        'Sanat ovat ruudukossa pysty- tai vaakasuorassa vinottain tai takaperin.',
+		
+        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.':
+        'Jos jokin sana tuntuu liian vaikealta, paina ratkaisu-nappia.',
+		
+		'Onnea!':
+        'Onnea!'
+
+       
+    
+});
