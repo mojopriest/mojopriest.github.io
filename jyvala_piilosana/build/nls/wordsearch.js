f8556c188b42f4c7ad89c8b1aa49ee0528ce1d7f
mojopriest <jari.realestate@gmail.com>
2026-05-17 19:01:39 +0300

    gg

--- jyvala_piilosana/build/nls/wordsearch.js
+++ jyvala_piilosana/build/nls/wordsearch.js
@@ -1,42 +1,17 @@
 define({
     root: {
-        'Pisteet %d':
-        'Pisteet %d',
-    
-        'Ohje':
-        'Ohje',
-        
-        'Uudelleen':
-        'Uudelleen',
-        
-        'Download':
-        'Download',
-
-        'ratkaisu':
-        'ratkaisu',
-        
-        'Sulje':
-        'Sulje',
-
-        'Hienoa!':
-        'Hienoa!',
-
-        'Pisteesi':
-        'Pisteesi',
-
-        'Ohje':
-        'Ohje',
-		
-		'Find and mark all the words inside the box before your points fall to zero.':
-        'XXX.',
-
-        'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.':
-        'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
-
-        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.':
-        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.',
-
-        'Good Luck':
-        'Onnea!'
+        'You have %d points': 'Pisteet %d',
+        'Help': 'Ohje',
+        'Restart': 'Uudelleen',
+        'Download': 'Download',
+        'solve': 'ratkaisu',
+        'Close': 'Sulje',
+        'Congratulations!': 'Hienoa!',
+        'Your score is': 'Pisteesi',
+        'How to play!': 'Ohje',
+        'Find and mark all the words inside the box before your points fall to zero.': 'Mitä nopeammin löydät kaikki sanat, sitä enemmän saat pisteitä.',
+        'The words may be horizontally, vertically, diagonally and even backwards.': 'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
+        'If you give up searching for a word use the button next to the word.': 'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.',
+        'Good Luck': 'Onnea!'
     }
 });
