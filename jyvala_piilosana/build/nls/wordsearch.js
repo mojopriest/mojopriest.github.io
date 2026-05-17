d91f0402d76bd8c3bd1bba376450ca9fc382d4e3
mojopriest <jari.realestate@gmail.com>
2026-05-17 18:09:24 +0300

    fdf

--- jyvala_piilosana/build/nls/wordsearch.js
+++ jyvala_piilosana/build/nls/wordsearch.js
@@ -27,16 +27,16 @@ define({
         'Ohje':
         'Ohje',
 		
-		'Find and mark all the words inside the box before your points fall to zero.':
-   	    'Find and mark all the words inside the box before your points fall to zero.',
+		'Etsi kaikki':
+        'Etsi kaikki',
 
-        'The words may be horizontally, vertically, diagonally and even backwards.':
+        'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.':
         'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
 
-        'If you give up searching for a word use the button next to the word.':
+        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.':
         'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.',
 
-        'Good Luck':
+        'Onnea!':
         'Onnea!'
     }
 });
