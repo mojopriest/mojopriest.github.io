9f2e89aded8a40eb9682208f023e751a2854ceeb
mojopriest <jari.realestate@gmail.com>
2026-05-17 16:19:51 +0300

    hh

--- jyvala_piilosana/build/nls/wordsearch.js
+++ jyvala_piilosana/build/nls/wordsearch.js
@@ -32,11 +32,15 @@ define({
 
         'Sanat ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.':
         'Sanat ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
+		
+		 'Mitä nopeammin löydät kaikki sanat, sitä enemmän saat pisteitä.':
+         'Mitä nopeammin löydät kaikki sanat, sitä enemmän saat pisteitä.',
+
 
         'Jos jokin sana tuntuu liian vaikealta, paina ratkaisu-nappia.':
         'Jos jokin sana tuntuu liian vaikealta, paina ratkaisu-nappia.',
 
         'Onnea!':
         'Onnea!'
-    },
+    }
 });
