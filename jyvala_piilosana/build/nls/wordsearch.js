5657af1de9cefeac5d7043998e4b06f6b72b356b
mojopriest <jari.realestate@gmail.com>
2026-05-17 19:29:59 +0300

    fdf

--- jyvala_piilosana/build/nls/wordsearch.js
+++ jyvala_piilosana/build/nls/wordsearch.js
@@ -31,8 +31,8 @@ define({
         'Sanat ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.':
         'Sanat ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
 
-        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.':
-        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.',
+        'Jos jokin sana tuntuu liian vaikealta, paina ratkaisu-nappia.':
+        'Jos jokin sana tuntuu liian vaikealta, paina ratkaisu-nappia.',
 
         'Onnea!':
         'Onnea!'
