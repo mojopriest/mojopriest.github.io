7f95044d14dadb7746719064e5311f0832adc1f0
mojopriest <jari.realestate@gmail.com>
2026-05-17 20:56:01 +0300

    vvbv

--- jyvala_piilosana/build/nls/template/wordsearch.js
+++ jyvala_piilosana/build/nls/template/wordsearch.js
@@ -1,41 +1,41 @@
 define({
-    'You have %d points':
-    'You have %d points',
-
-    'Help':
-    'Help',
+        'Pisteet %d':
+        'Pisteet %d',
     
-    'Restart':
-    'Restart',
+        'Ohje':
+        'Ohje',
+        
+        'Uudelleen':
+        'Uudelleen',
+        
+        'Download':
+        'Download',
+
+        'ratkaisu':
+        'ratkaisu',
+        
+        'Sulje':
+        'Sulje',
+
+        'Hienoa!':
+        'Hienoa!',
+
+        'Pisteesi':
+        'Pisteesi',
+
+        'Ohje':
+        'Ohje',
+		
+		'Etsi kaikki':
+        'Etsi kaikki',
+
+        'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.':
+        'Nimet ovat ruudukossa pysty- tai vaakasuorassa, vinottain tai takaperin.',
+
+        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.':
+        'Jos jokin nimi tuntuu liian vaikealta, paina ratkaisu-nappia.',
+
+        'Onnea!':
+        'Onnea!'
     
-    'Download':
-    'Download',
-
-    'solve':
-    'solve',
-    
-    'Close':
-    'Close',
-
-    'Congratulations!':
-    'Congratulations!',
-
-    'Your score is':
-    'Your score is',
-
-    'How to play!':
-    'How to play!',
-
-    'Find and mark all the words inside the box before your points fall to zero.':
-    'Find and mark all the words inside the box before your points fall to zero.',
-
-    'The words may be horizontally, vertically, diagonally and even backwards.':
-    'The words may be horizontally, vertically, diagonally and even backwards.',
-
-    'If you give up searching for a word use the button next to the word.':
-    'If you give up searching for a word use the button next to the word.',
-
-    'Good Luck':
-    'Good Luck'
-
 });
